3d5bbce00bc22011e5d1e8285134ef6968c96a9a
nan nan <nnaapp@qq.com>
2019-01-15 21:45:14 +0000

    welcome test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel-gxipb 01 15
+                    Laravel-github test
                 </div>
 
                 <div class="links">
